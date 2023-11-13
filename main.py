08aa0f255c0b52bfd1c161a145e042d36aabb792
Vadimzayats <144005524+Vadimzayats@users.noreply.github.com>
2023-11-13 09:05:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-#1 Задание
 s = 0
 k = 15
-d = k-10
-def ff():
-    k = 4*d
-    s = k-50
-    print(s)
-ff()
-
-#2 Задание
-
+d = k - 10
+k = 4 * d
+s = k - 50
+print (s)
+#2
 x = 3
 y = 4
 z = x + y
@@ -18,151 +13,256 @@ x = y
 y = 5
 x = z + y + 7
 print(x)
-
-#3 Задание
-
-n = int(input())
-for k in range(n, n + 3):
-    print(k)
-
-#4 Задание
-
-p = int(input())
-o = int(input())
-i = int(input())
-m = p+o+i
-print(m)
-
-#5 Задание
-
-l = int(input())
-def ss():
-    V = l**3
-    s = 6*l**2
-    print(V)
-    print(s)
-ss()
-
-#6 Задание
-
-g = int(input())
-h = int(input())
-def zf():
-    q = 3*(g+h)**3+275*h**2-127*g-41
-    print(q)
-zf()
-
-#7 Задание
-t = int(input())
-def ps():
-    global t
-    y = t + 1
-    print("Следующее за числом ", t, "число ", y)
-ps()
-def pp():
-    global t
-    py = t - 1
-    print("Предыдущее за числом ", t, "число ", py)
-pp()
-
-#8 Задание
-pc = int(input())
-mnt = int(input())
-klv = int(input())
-msh = int(input())
-def stoim():
-    stoma = (pc+mnt+klv+msh)*3
-    print(stoma)
-stoim()
-#9 zadanie
-a = int(input())
-b = int(input())
-def i ():
-    z = a+b
-    x = a-b
-    c = a*b
-    print(a, "+", b, "=", z)
-    print(a, "-", b, "=", x)
-    print(a, "*", b, "=", c)
-i()
-# 10
-c = int(input())
-b = int(input())
-a = int(input())
-def i():
-    ar = (2 * a + (b - 1) * c) * b / 2
-    print(int(ar))
-i()
-11
-x = int(input())
-def i():
-    print(x,"---", x*2,"---", x*3,"---", x*4,"---", x*5)
-i()
-
-#12
-a = int(input())
-b = int(input())
-c = int(input())
-def i():
-    sum = a*b**(c-1)
-    print(sum)
-i()
-
-#13
-a = int(input())
-def i ():
-    z = a/100
-    print(int(z))
-i()
-
-#14
-n = int(input())
-k = int(input())
-def i ():
-    print(k%n)
-i()
-
-#15
-a = int(input())
-if a % 2 == 0:
-    v = a/2
-    print(int(v))
+#3
+n = int(input(''))
+print(' Первое число:',n,'\n','Второе число:',n+1,'\n','Третье число:',n+2,'\n')
+#4
+n = int(input('Число дайте'))
+x = int(input('Число дайте'))
+t = int(input('Число дайте'))
+print(n+x+t)
+#5
+n = int(input('длина ребра'))
+print('Площадь полной поверхности:',6*n**2,'\n','Объем:',n**3)
+#6
+a = int(input('First number a'))
+b = int(input('Second number b'))
+print('3*(a+b)^3 + 275*b^2 - 127*a - 41 =', 3*(a+b)**3 + 275*b**2 - 127*a -41)
+#7
+n = int(input('Your number:'))
+print('Следущее за числом',n,'число:',n+1)
+print('Для числа',n,'предыдущее число:',n-1)
+#8
+n = int(input('Цена1'))
+b = int(input('Цена2'))
+c = int(input('Цена3'))
+e = int(input('Цена4'))
+print((n+b+c+e)*3)
+#9
+x = int(input('number 1'))
+y = int(input('number 2'))
+print(x+y,'suma \n', x*y,'proizvedenie \n',x-y, 'raznostb \n')
+#10
+y = int(input('Pervoe chislo'))
+x = int(input('vtoroe chislo'))
+n = int(input('tretbe chislo'))
+print(((2*n+(x-1)*y)*x/2))
+#11
+n = int(input('Chislo'))
+if n > 0:
+    print(n,'---',2*n,'---',3*n,'---',4*n,'---',5*n,)
 else:
-    b = (a+1)/2
-    print(int(b))
-
+    print('прикол')
+#12
+y = int(input('Pervoe chislo'))
+x = int(input('vtoroe chislo'))
+n = int(input('tretbe chislo'))
+print(y*x**(n-1))
+#13
+n = int(input('Number'))
+if n > 0:
+    print(n//100)
+else:
+    print('прикол')
+#14
+n = int(input('Wkolota'))
+k = int(input('Mandariniiii'))
+print(k//n,'Celoe chislo mandarinov wolnikam\n',k%n,'v korzine')
+#15
+n = int(input('Naselenie'))
+if n % 2 != 0:
+    print(n//2+1)
+else:
+    print(n//2)
 #16
-n = int(input())
-if n % 4 > 0:
-    print(n//4+1)
-if n % 4 == 0:
-    print(n//4)
-
+n = int(input('nomer'))
+print(n//4+1)
 #17
-a = int(input())
-print(a, "мин - это ", a//60, "час", a%60, "минут.")
-
+n = int(input('minuts'))
+hours = n//60
+minut = n%60
+print(hours,'hours',minut,'minut')
 #18
-a = list(input())
-n = int(a[0]) + int(a[1]) + int(a[2])
-n2 = int(a[0]) * int(a[1]) * int(a[2])
-print("Сумма цифр = ", n)
-print("Произведение цифр = ", n2)
-
+n = int(input('chislo'))
+f = n//100
+s = n//10%10
+l = n%10
+print(f+s+l,f*s*l)
 #19
-a = list(input())
-n = str(a[0]) + str(a[1]) + str(a[2])
-n2 = str(a[0]) + str(a[2]) + str(a[1])
-n3 = str(a[1]) + str(a[0]) + str(a[2])
-n4 = str(a[1]) + str(a[2]) + str(a[0])
-n5 = str(a[0]) + str(a[1]) + str(a[2])
-n6 = str(a[2]) + str(a[0]) + str(a[1])
-print(n)
-print(n2)
-print(n3)
-print(n4)
-print(n5)
-print(n6)
-
+n = int(input('chislo'))
+f = n // 100
+s = n // 10 % 10
+l = n % 10
+f = str(f)
+s = str(s)
+l = str(l)
+print(f+s+l)
+print(f+l+s)
+print(s+f+l)
+print(s+l+f)
+print(l+f+s)
+print(l+s+f)
 #20
-
+n = int(input('chislo'))
+print(n//1000,n%1000//100,n%100//10,n%10)
+#21
+n = input('password')
+s = input('password2')
+if n == s:
+    print('Password is true')
+else:
+    print('Password is false')
+#22
+n = int(input('chislo'))
+if n % 2 !=0:
+    print('Нечетное')
+else:
+    print('Четное')
+#23
+n = int(input('chislo'))
+a = n // 1000
+b = n % 1000 // 100
+c = n % 100 // 10
+d = n % 10
+if a+d == b-c:
+    print('DA')
+else:
+    print('NET')
+#24
+n = int(input('age'))
+if n >= 0:
+    print('Mozno')
+else:
+    print('Nelzya')
+#25
+n = int(input('chislo1'))
+b = int(input('chislo2'))
+d = b-n
+c = int(input('chislo3'))
+if c == b + d:
+    print('DA')
+else:
+    print('NET')
+#26
+n = int(input('chislo1'))
+b = int(input('chislo2'))
+if n - b > 0:
+    print(n)
+else:
+    print(b)
+#27
+n = int(input('chislo1'))
+b = int(input('chislo2'))
+c = int(input('chislo3'))
+a = int(input('chislo4'))
+if n - b > 0 and n - c > 0 and n - a >0:
+    print(n)
+elif b - c > 0 and b - a >0:
+    print(b)
+elif c - a > 0:
+    print(c)
+else:
+    print(a)
+#28
+n = int(input('age'))
+if n <= 13:
+    print('Baby')
+elif n>=14 and n<=24:
+    print('Molodoi')
+elif n>=25 and n<=59:
+    print('zrelostb')
+else:
+    print('storostb')
+#29
+sum = 0
+i = 0
+while i < 3:
+    n = int(input('chislo'))
+    i = i+1
+    if n>0:
+        sum= sum+n
+print(sum)
+#30
+n = int(input('chislo'))
+if n//1000 !=0 and n%7 ==0 or n%17 ==0:
+    print('YES')
+else:
+    print('NO')
+#31
+n = int(input('chislo1'))
+b = int(input('chislo2'))
+c = int(input('chislo3'))
+if n + b > c and n+c>b and b+c > n:
+    print('YES')
+else:
+    print('NO')
+#32
+n = int(input('age'))
+if n % 4 == 0 and n % 100 !=0:
+    print('YES')
+else:
+    print('NO')
+#33
+f1 = int(input('stolb1'))
+n1 = int(input('strok1'))
+f2 = int(input('stolb2'))
+n2 = int(input('strok2'))
+if f1 == f2 or n1 == n2:
+    print('YES')
+else:
+    print('NO')
+#34
+f1 = int(input('stolb1'))
+n1 = int(input('strok1'))
+f2 = int(input('stolb2'))
+n2 = int(input('strok2'))
+if (f1 == f2+1 or f1 == f2-1 or f1==f2) and (n1 == n2 or n1 == n2+1 or n1==n2-1):
+    print('YES')
+else:
+    print('NO')
+#35
+a = int(input(''))
+b = int(input(''))
+c = (input(''))
+if c == '+':
+    print(a+b)
+elif c == '-':
+    print (a-b)
+elif c == '*':
+    print(a*b)
+elif c == '/':
+    if b != 0:
+        print(a/b)
+    else:
+        print('На 0 делить нельзя!')
+else:
+    print('Неверная операция!')
+#36
+n = int(input('Zum'))
+k = int(input('Flash'))
+if n > k:
+    print('NO')
+elif n < k:
+    print('YES')
+else:
+    print('Dont know')
+#37
+a = int(input())
+b = int(input())
+c = int(input())
+if (a == b and b != c and a != c) or (a == c and a != b and c != b) or (b == c and b != a and c != a):
+    print('Равнобедренный')
+elif a == b and a == c and c == b:
+    print('Равносторонний')
+else:
+    print('Разносторонний')
+#38
+a = int(input())
+b = int(input())
+c = int(input())
+if (a > b and a < c) or (a > c and a < b):
+    print(a)
+elif (b > a and b < c) or (b > c and b < a):
+    print(b)
+else:
+    print(c)
